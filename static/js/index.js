68187ada1dc25c01f20fd37ef93fc9fc317d2524
gengbiao <gengbiao@weejinfu.com>
2017-11-22 23:42:31 +0800

    51credit

---
+++ static/js/index.js
@@ -0,0 +1,16 @@
+(function () {
+   // console.log($)
+
+    // $.ajax({
+    //     url : 'https://cnodejs.org/api/v1/topics',
+    //     type : 'get',
+    //     // data : {
+    //     //     userId : userId,
+    //     //     bdId : staffId
+    //     // },
+    //     dataType : 'json',
+    //     success : function(data) {
+    //         console.log(JSON.stringify(data))
+    //     }
+    // })
+})()
