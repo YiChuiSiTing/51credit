76f17fe6ed077b0be9891b80d3a6fdfa8fa7a427
327400117@qq.com <327400117@qq.com>
2017-11-29 22:46:58 +0800

    test

---
+++ static/js/buyCurrentGold.js
@@ -0,0 +1,168 @@
+
+$(function () {
+
+
+    //takeGrams
+
+    //showCounter
+
+})
+var buyCurrentGold = {
+    init:function () {
+        this.changeSelected();
+        this.onfucous();
+        this.switchGrams();
+        this.changePrice();
+        this.buy();
+    },
+    changeSelected:function () {
+        //changeSelected
+        $(".checkImg").click(function () {
+            if($(this).attr("src") == '../static/img/noRead.png'){
+                $(this).attr("src",'../static/img/read.png')
+            }else{
+                $(this).attr("src",'../static/img/noRead.png')
+            }
+        })
+    },
+    onfucous:function () {
+        //onfocus
+        var that = this;
+        $(".numInput").on('input',function () {
+            if($(this).val() == ''){
+                $(".RMB").hide();
+                $(".arrow_box").hide();
+                $(".keepDown").attr("disabled","disabled").css("opacity",'0.45')
+            }else{
+                $(".keepDown").removeAttr("disabled").css("opacity",'1')
+                $(this).val(util.amount($(this).val()))
+                $(".RMB").show();
+                $(this).css("color","#333");
+                $(".arrow_box").show();
+                if($('.switchGrams').html() == '切换成按克重'){
+                    that.takeGrams($(".numInput").val())
+                }else{
+                    that.takeMoney($(".numInput").val());
+                    $(".RMB").hide()
+                }
+            }
+        })
+    },
+    takeGrams:function (val) {
+        var rgpPrice = parseFloat($(".rgpPrice").text());
+        var addPrice = parseFloat($(".addPrice").text());
+        var grams = val / (rgpPrice + rgpPrice)
+        $(".grams").text(grams.toFixed(2));
+        this.balanceTips(val)
+    },
+    switchGrams:function () {
+        $(".switchGrams").click(function () {
+            $(".numInput").val("");
+            $(".arrow_box").hide();
+            $(".balanceMes").hide();
+            if($(this).text() == '切换成按克重'){
+                $(".RMB").hide()
+                $(this).text('切换成按金额');
+                $(".numInput").attr("placeholder","请输入购买克数")
+            }else{
+                $(this).text('切换成按克重')
+                $(".numInput").attr("placeholder","请输入购买金额")
+            }
+        })
+    },
+    takeMoney:function (val) {
+        var rgpPrice = parseFloat($(".rgpPrice").text());
+        var addPrice = parseFloat($(".addPrice").text());
+        var money = val * rgpPrice + val * rgpPrice;
+        $(".logo").html('预估金额<span class="titleMoney">' + money+ '</span>元');
+        this.balanceTips(val)
+    },
+    balanceTips:function (val) {
+        var balanceMoney = parseFloat($(".balanceMoney").text());
+        var titleMoney = parseFloat($(".titleMoney").text());
+        if(val > balanceMoney || titleMoney > balanceMoney || val == 0){
+            $(".balanceMes").show()
+            $(".keepDown").attr("disabled","disabled").css("opacity",'0.45')
+        }else{
+            $(".balanceMes").hide()
+        }
+    },
+    changePrice:function () {
+        $(".changePrice").click(function () {
+            $(".payMesBox").show();
+            $(".showChangeBox").show();
+            $("#payBox").hide();
+        });
+        this.cancle();
+        this.enter();
+    },
+    buy:function () {
+        $(".keepDown").click(function () {
+            $(".payMesBox").show();
+            $(".showChangeBox").hide();
+            $("#payBox").show();
+        });
+    },
+    cancle:function () {
+        $("#cancle,.close").click(function () {
+            $(".payMesBox").hide();
+        })
+    },
+    enter:function () {
+        $("#enter").click(function () {
+            $(".showChangeBox").hide();
+            $("#payBox").show();
+            $(".payMesBox").hide();
+        })
+        this.surePay();
+    },
+    surePay:function () {
+        $(".surePay").click(function () {
+            $(".alertTips").show();
+        })
+        this.know();
+    },
+    know:function () {
+        $(".know").click(function () {
+            $(".fullScreen").hide();
+        })
+    },
+    plus:function () {
+        var num=0.1;
+        var price=Number($(".addPrice").text());
+        var saprice=Number($(".price").text())
+        $(".addPrice").text(buyCurrentGold.accAdd(price,num));
+        $('.price').text(buyCurrentGold.accAdd(saprice,num))
+    },
+    minus:function () {
+            var num=0.1;
+            var price=Number($(".addPrice").text());
+            var saprice=Number($(".price").text())
+            if($(".addPrice").text()<=0.5){
+                $(".addPrice").text(0.5);
+                $('.price').text(0.5)
+            }else{
+                $(".addPrice").text(buyCurrentGold.Subtr(price,num));
+                $('.price').text(buyCurrentGold.Subtr(saprice,num))
+            }
+
+    },
+    accAdd:function (arg1,arg2){
+        var r1,r2,m;
+        try{r1=arg1.toString().split(".")[1].length}catch(e){r1=0}
+        try{r2=arg2.toString().split(".")[1].length}catch(e){r2=0}
+        m=Math.pow(10,Math.max(r1,r2))
+        return (arg1*m+arg2*m)/m
+    },
+    Subtr:function (arg1,arg2){
+        var r1,r2,m,n;
+        try{r1=arg1.toString().split(".")[1].length}catch(e){r1=0}
+        try{r2=arg2.toString().split(".")[1].length}catch(e){r2=0}
+        m=Math.pow(10,Math.max(r1,r2));
+        n=(r1>=r2)?r1:r2;
+        return ((arg1*m-arg2*m)/m).toFixed(n);
+    }
+
+
+}
+buyCurrentGold.init()
---
+++ static/js/util.js
@@ -0,0 +1,33 @@
+
+var util = {
+    goReviousPage: function () {
+        window.history.go(-1)
+    },
+    amount: function (v) {
+        var regStrs = [
+            // ['^0(\\d+)$', '$1'], //禁止录入整数部分两位以上，但首位为0
+            ['[^\\d\\.]+$', ''], //禁止录入任何非数字和点
+            ['\\.(\\d?)\\.+', '.$1'], //禁止录入两个以上的点
+            ['^(\\d+\\.\\d{2}).+', '$1'] //禁止录入小数点后两位以上
+        ];
+        for (var i = 0; i < regStrs.length; i++) {
+            var reg = new RegExp(regStrs[i][0]);
+            v = v.replace(reg, regStrs[i][1]);
+        }
+        return v;
+    },
+    refreshPrice:function (ele) {
+        var that=this;
+        window.setInterval(function () {
+            ele.text(that.getPrice())
+        },1000)
+    },
+    getPrice:function () {
+        return Math.floor(Math.random()*100)+300;
+    },
+    
+    
+}
+$("#titleBarBack").click(function () {
+    util.goReviousPage()
+})
